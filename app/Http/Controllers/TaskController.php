4d2c2e0d893fadc1de6a0acbf7c9a4eb3cf2bdca
user <user@user.com>
2017-09-25 21:07:00 +0300

    Отображение задач

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,19 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Repositories\TaskRepository;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 
 class TaskController extends Controller {
 
     /**
+     * Экземпляр TaskRepository.
+     *
+     * @var TaskRepository
+     */
+    protected $tasks;
+
+    /**
      * Создание нового экземпляра контроллера.
      *
      * @return void
      */
-    public function __construct() {
+    public function __construct(TaskRepository $tasks) {
 	$this->middleware('auth');
+	$this->tasks = $tasks;
     }
 
     /**
@@ -24,6 +32,29 @@ class TaskController extends Controller {
      */
     public function index(Request $request) {
 	return view('tasks.index');
+	$tasks = $request->user()->tasks()->get();
+	return view('tasks.index', [
+	    'tasks' => $tasks,
+	]);
+    }
+
+    /**
+     * Создание новой задачи.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request) {
+	$this->validate($request, [
+	    'name' => 'required|max:255',
+	]);
+
+	// Создание задачи...
+	$request->user()->tasks()->create([
+	    'name' => $request->name,
+	]);
+
+	return redirect('/tasks');
     }
 
 }
